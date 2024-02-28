a1540338590bcf750a5935a03555ed7776a4a2a6
Prerit Khandelwal <prerit.web@gmail.com>
2024-02-28 16:52:21 +0530

    online db added

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_LOCAL_URL);
+    const conn = await mongoose.connect(process.env.MONGO_ONLINE_URL);
     console.log(
       `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
         .white
